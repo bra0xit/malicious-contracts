61abc5ec312a56fd7f27382dd6a1e9ed05336efe
alexevander <ax.evander@gmail.com>
2023-08-04 14:57:46 +0200

    first commit

---
+++ contracts/Good.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import "./Helper.sol";
+
+contract Good {
+    Helper public helper;
+    constructor() payable {
+        helper = new Helper();
+    }
+
+    function isUserEligible() public view returns(bool) {
+        return helper.isUserEligible(msg.sender);
+    }
+
+    function addUserToList() public  {
+        helper.setUserEligible(msg.sender);
+    }
+
+    fallback() external {}
+}
